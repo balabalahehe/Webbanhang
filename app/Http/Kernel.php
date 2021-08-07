e2a772536be54a36de8b30cc7056d01b15e23d61
Nguyễn Quốc Long <quoclongdng@gmail.com>
2021-08-07 08:52:54 +0700

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,6 +52,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
+    
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
